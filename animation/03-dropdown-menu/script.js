ddd6263d24546c1648f857921a7ec92591db2af1
rizk <rizkdotai@gmail.com>
2024-01-26 16:07:16 +0200

    add animation

--- animation/03-dropdown-menu/script.js
+++ animation/03-dropdown-menu/script.js
@@ -2,12 +2,15 @@ const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
+// open/close dropdown if user clicks on menu title 
+
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
     dropdownMenu.classList.toggle("visible");
   }  
 })
 
+// close dropdown if user clicks anywhere outside dropdown container
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
     dropdownMenu.classList.remove("visible")
